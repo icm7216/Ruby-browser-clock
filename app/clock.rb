7e65296d878329d59733d3111496a3ef3138efd9
icm7216 <icm7216@users.noreply.github.com>
2019-02-28 02:03:41 +0900

    add FPS, moving average filter

--- app/clock.rb
+++ app/clock.rb
@@ -13,9 +13,20 @@ module Clock
   @canvas.width = '800'
   @canvas.style.backgroundColor = 'rgb(3,21,192)'
   
-  def self.view(time)
+  def self.maf(data, tap = 15)
+    @tap = tap
+    @n ||= 0
+    @xn ||= Array.new(@tap, 0)
+    @xn[@n] = data
+    @n = (@n + 1) % @tap
+    @xn.sum / @tap
+  end
+
+  def self.view(time, fps)
     @ctx.clearRect(0, 0, 800, 200)
     @ctx.fillStyle = 'rgb(252,167,39)'
+    @ctx.font = '30px consolas'
+    @ctx.fillText(fps, 10, 30)
     @ctx.font = '100px consolas'
     @ctx.fillText(time, 70, 130)
   end
@@ -24,8 +35,11 @@ module Clock
     render = -> do
       $$[:requestAnimationFrame][render]
       cur_t = Time.now
+      fps_t = 1.0 / (cur_t.to_f - @pre_t.to_f)
+      fps = "FPS: #{sprintf('%.2f', maf(fps_t))}"
       time = cur_t.strftime('%H:%M:%S.%L')
-      view(time)
+      @pre_t = cur_t
+      view(time, fps)      
     end
     render[]
   end
